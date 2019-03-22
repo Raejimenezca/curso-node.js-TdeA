ed8e00d80db923eae56315833c603912588a0fce
raejimenezca <raejimenezca@unal.edu.co>
2019-03-22 16:50:36 -0500

    Proyecto 1

---
+++ datos.js
@@ -0,0 +1,26 @@
+const opciones = {
+    matematicas :{
+        default: 0,
+        alias: 'm'
+    },
+    ingles: {
+        default: 0,
+        alias: 'i'
+    },
+    programacion: {
+        demand: true, // default: pone un valor por defecto cuando no se define, 'demand': muestra las opciones disponibles y obliga a ingresar un valor 
+        alias: 'p'
+    }
+}
+
+const argv = require('yargs')
+    .command('promedio', 'Calcular el promedio', opciones)
+    .argv;
+
+
+let obtenerPromedio = (nota_uno, nota_dos, nota_tres) => (nota_uno + nota_dos + nota_tres) / 3;
+
+module.exports = {
+    estudiante,
+    obtenerPromedio
+}
